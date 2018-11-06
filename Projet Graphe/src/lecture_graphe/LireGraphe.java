8e1447d8636ff9b7452617a776e0c43431edf0d7
Utilisateur <Utilisateur@192.168.0.24>
2018-11-06 01:44:22 +0100

    suppression de la classe ReadFile (doublon)

--- Projet Graphe/src/lecture_graphe/LireGraphe.java
+++ Projet Graphe/src/lecture_graphe/LireGraphe.java
@@ -1,10 +1,23 @@
 package lecture_graphe;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class LireGraphe {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		if (args.length != 1)
+		{
+			throw new IllegalArgumentException("Un seul argument attendu");
+		}
+		
+		Scanner scan = new Scanner(new File(args[0]));
+		
+		while (scan.hasNext())
+		{
+			scan.nextLine();
+		}
 	}
 
 }
